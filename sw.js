7b047c3688851a1d1e25f519b0196539c37174e5
Mahdii <mahhhdi1223@gmail.com>
2026-09-01 03:11:40 +0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "installment-ledger-v1";
+const CACHE_NAME = "installment-ledger-v2";
 const APP_SHELL = ["./", "./index.html", "./manifest.json", "./icon-192.png", "./icon-512.png"];
 
 self.addEventListener("install", (event) => {
